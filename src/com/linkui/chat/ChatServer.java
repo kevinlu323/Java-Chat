742b0ad155016dc3082d72151ea50baf112ca1e2
linkui <linkui@192.168.1.115>
2015-08-15 15:43:50 -0500

    v1.0 - Update server, use Thread to handle each client

--- src/com/linkui/chat/ChatServer.java
+++ src/com/linkui/chat/ChatServer.java
@@ -4,42 +4,77 @@ import java.io.*;
 import java.net.*;
 
 public class ChatServer {
+	boolean started = false;
+	ServerSocket ss = null;
+	Socket s = null;
+	
 	public static void main(String[] args){
-		boolean started = false;
-		ServerSocket ss = null;
-		Socket s = null;
-		DataInputStream dis = null;
+		new ChatServer().start();
+	}
+	
+	public void start(){
 		try {
 			ss = new ServerSocket(8888);
+			started = true;
 		} catch (BindException e){
 			System.out.println("Port is in use by others");
+			System.exit(-1);
 		} catch (IOException e){
 			e.printStackTrace();
 		}
-		try{
-			started = true;
-			while(started){
-				boolean isConnected = false;
+		
+		while(started){
+			try {
 				s = ss.accept();
-				isConnected = true;
 				System.out.println("A client is connected");
-				dis = new DataInputStream(s.getInputStream());
-				while(isConnected){
-					String str = dis.readUTF();
-					System.out.println(str);
+				Client c = new Client(s);
+				new Thread(c).start();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					ss.close();
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
 			}
-		} catch (EOFException e) {
-			System.out.println("Client is disconnected.");
-		} catch (IOException e){
-			e.printStackTrace();
-		} finally {
-			try{
-				if(dis != null) dis.close();
-				if(s != null) s.close();
+		}
+	}
+	
+	class Client implements Runnable{
+		private Socket s = null;
+		private DataInputStream dis = null;
+		private boolean isConnected = false;
+		
+		Client (Socket s){
+			this.s = s;
+			try {
+				dis = new DataInputStream(s.getInputStream());
+				isConnected = true;
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			catch (IOException e1){
-				e1.printStackTrace();
+		}
+		
+		@Override
+		public void run() {
+			try{
+			while(isConnected){
+				String str = dis.readUTF();
+				System.out.println(str);
+			}
+			} catch (EOFException e) {
+				System.out.println("Client is disconnected.");
+			} catch (IOException e){
+				e.printStackTrace();
+			} finally {
+				try{
+					if(dis != null) dis.close();
+					if(s != null) s.close();
+				}
+				catch (IOException e1){
+					e1.printStackTrace();
+				}
 			}
 		}
 	}
