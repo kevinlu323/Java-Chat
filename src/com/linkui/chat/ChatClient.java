7b91ad2d2a25d2f1c02b2b7998e30b09eb63c65d
linkui <linkui@192.168.1.115>
2015-08-15 12:03:42 -0500

    v0.2 - Add TextField and TextArea

--- src/com/linkui/chat/ChatClient.java
+++ src/com/linkui/chat/ChatClient.java
@@ -3,6 +3,9 @@ package com.linkui.chat;
 import java.awt.*;
 
 public class ChatClient extends Frame{
+	TextField tf = new TextField();
+	TextArea ta = new TextArea();
+	
 	public static void main(String[] args){
 		new ChatClient().launchFrame();
 	}
@@ -10,6 +13,9 @@ public class ChatClient extends Frame{
 	public void launchFrame(){
 		this.setLocation(200, 300);
 		this.setSize(300, 300);
+		this.add(tf, BorderLayout.SOUTH);
+		this.add(ta, BorderLayout.NORTH);
+		pack();
 		this.setVisible(true);
 	}
 }
