79ce1a786d7bf76bc4b29af7be3163d0e9ac9f6b
linkui <linkui@192.168.1.115>
2015-08-16 16:56:23 -0500

    v1.2 - Client can show message from others, Exception is handled.

--- src/com/linkui/chat/ChatClient.java
+++ src/com/linkui/chat/ChatClient.java
@@ -8,8 +8,11 @@ import java.net.*;
 public class ChatClient extends Frame{
 	TextField tf = new TextField();
 	TextArea ta = new TextArea();
-	Socket s;
-	DataOutputStream dos;
+	Socket s = null;
+	DataInputStream dis = null;
+	DataOutputStream dos = null;
+	private boolean isConnected = false;
+	Thread receiver = new Thread(new ClientReceiver());
 	
 	public static void main(String[] args){
 		new ChatClient().launchFrame();
@@ -32,12 +35,15 @@ public class ChatClient extends Frame{
 		tf.addActionListener(new TFListener());
 		this.setVisible(true);
 		this.connect();
+		receiver.start();
 	}
 	
 	public void connect(){
 		try {
 			s = new Socket("127.0.0.1",8888);
+			isConnected = true;
 			dos = new DataOutputStream(s.getOutputStream());
+			dis = new DataInputStream(s.getInputStream());
 			System.out.println("Connected to server");
 		} catch (UnknownHostException e) {
 			System.out.println("Cannot fing server!");
@@ -49,6 +55,9 @@ public class ChatClient extends Frame{
 	
 	public void disconnect(){
 		try{
+			isConnected = false;
+			receiver.interrupt();
+			dis.close();
 			dos.close();
 			s.close();
 		} catch (IOException e){
@@ -60,13 +69,28 @@ public class ChatClient extends Frame{
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			String str = tf.getText().trim();
-			ta.setText(str);
+			ta.append("I said: " + str + "\n");
 			tf.setText("");
 			try {
 				dos.writeUTF(str);
 				dos.flush();
 			} catch (IOException e1) {
 				e1.printStackTrace();
+			}
+		}
+	}
+	
+	private class ClientReceiver implements Runnable{
+
+		@Override
+		public void run() {
+			try{
+				while(Thread.currentThread().isInterrupted() && isConnected){
+					String str = dis.readUTF();
+					ta.append("Others Said: " + str + "\n");
+				}
+			} catch (IOException e2){
+				e2.printStackTrace();
 			}
 		}
 		
--- src/com/linkui/chat/ChatServer.java
+++ src/com/linkui/chat/ChatServer.java
@@ -9,48 +9,48 @@ public class ChatServer {
 	ServerSocket ss = null;
 	Socket s = null;
 	List<Client> clients = new ArrayList<>();
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		new ChatServer().start();
 	}
-	
-	public void start(){
+
+	public void start() {
 		try {
 			ss = new ServerSocket(8888);
 			started = true;
-		} catch (BindException e){
+		} catch (BindException e) {
 			System.out.println("Port is in use by others");
 			System.exit(-1);
-		} catch (IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		while(started){
-			try {
+
+		try { //try need to be added outside while() loop
+			while (started) {
 				s = ss.accept();
 				System.out.println("A client is connected");
 				Client c = new Client(s);
 				new Thread(c).start();
 				clients.add(c);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				ss.close();
 			} catch (IOException e) {
 				e.printStackTrace();
-			} finally {
-				try {
-					ss.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
 			}
 		}
 	}
-	
-	class Client implements Runnable{
+
+	class Client implements Runnable {
 		private Socket s = null;
 		private DataInputStream dis = null;
 		private DataOutputStream dos = null;
 		private boolean isConnected = false;
-		
-		Client (Socket s){
+
+		Client(Socket s) {
 			this.s = s;
 			try {
 				dis = new DataInputStream(s.getInputStream());
@@ -60,37 +60,39 @@ public class ChatServer {
 				e.printStackTrace();
 			}
 		}
-		
-		public void send(String sentStr){
+
+		public void send(String sentStr) {
 			try {
 				dos.writeUTF(sentStr);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		@Override
 		public void run() {
-			try{
-			while(isConnected){
-				String str = dis.readUTF();
-				System.out.println(str);
-				//Will not use Iterator here, Object lock is not needed.
-				for (int i = 0; i < clients.size(); i++){
-					clients.get(i).send(str);
+			try {
+				while (isConnected) {
+					String str = dis.readUTF();
+					System.out.println(str);
+					// Will not use Iterator here, Object lock is not needed.
+					for (int i = 0; i < clients.size(); i++) {
+						clients.get(i).send(str);
+					}
 				}
-			}
 			} catch (EOFException e) {
 				System.out.println("Client is disconnected.");
-			} catch (IOException e){
+			} catch (IOException e) {
 				e.printStackTrace();
 			} finally {
-				try{
-					if(dis != null) dis.close();
-					if(dos != null) dos.close();
-					if(s != null) s.close();
-				}
-				catch (IOException e1){
+				try {
+					if (dis != null)
+						dis.close();
+					if (dos != null)
+						dos.close();
+					if (s != null)
+						s.close();
+				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
 			}
